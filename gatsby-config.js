eadafff4ab1b04d44668b0024f8fdda86a942a74
Pedro Azevedo <pedro.azevedo@bemobi.com.br>
2019-09-10 18:29:18 -0300

    cleanup start stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `blog`,
+    description: `blog talking about everything`,
+    author: `@eu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
