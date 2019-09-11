acb2946299b63e224f67780bd24d2dc3cd502f3b
Pedro Azevedo <pedro.azevedo@bemobi.com.br>
2019-09-10 21:04:24 -0300

    section 5

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+// import { Container } from './styles';
+
+const Avatar = () => {
+  const { avatarImage } = useStaticQuery(
+    graphql`
+      query {
+        avatarImage: file(relativePath: { eq: "profile-photo.png" }) {
+          childImageSharp {
+            fixed(width: 60, height: 60) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `
+  )
+  return <Img fixed={avatarImage.childImageSharp.fixed} />
+}
+
+export default Avatar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <Link to="/about">about</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
