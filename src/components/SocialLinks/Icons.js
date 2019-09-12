cd0d872f9b0cce4a2c0ea531bf187885d37bb6e6
Pedro Azevedo <theazevedo@outlook.com>
2019-09-12 02:08:36 -0300

    update changes

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,11 @@
+import { Github } from "styled-icons/boxicons-logos/Github"
+import { Twitter } from "styled-icons/boxicons-logos/Twitter"
+import { Instagram } from "styled-icons/boxicons-logos/Instagram"
+
+const Icons = {
+  Github,
+  Twitter,
+  Instagram,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,17 @@
+const handle = "AzevedoDev"
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}`,
+  },
+]
+
+export default links
