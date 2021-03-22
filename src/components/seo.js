54523e544a1c9aa3b184abe08714ce6f341a89aa
Pedro Azevedo <theazevedo@outlook.com>
2021-03-22 03:27:48 -0300

    add og image

--- src/components/seo.js
+++ src/components/seo.js
@@ -28,7 +28,7 @@ function SEO({ description, lang, meta, title, image }) {
   const metaDescription = description || site.siteMetadata.description
 
   const url = site.siteMetadata.siteUrl
-  const ogImage = `${url}${image || "/assets/img/cover.png"}`
+  const ogImage = `${url}${image || "/assets/img/banner.png"}`
 
   return (
     <Helmet
